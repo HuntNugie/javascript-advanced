47db6af8563fd7e30c52a05eeb643c021a7f42f1
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-08 18:00:02 +0700

    tagged template literal

---
+++ tagTemplateLiteral/script.js
@@ -0,0 +1,17 @@
+// tag template literal berfungsi untuk membuat template literal di pinggir nya bisa menjadi parameter di function tag nya
+
+let Highlight = function(strings,...value)
+{  
+    return strings.reduce((hasil,str,index)=>{
+        return `${hasil}${str}${value[index] || ''}`
+},'') 
+}
+
+
+nama = "Nugie kurniawan"
+let umur = 21
+let email = "nugiekurniawan@gmail.com"
+let str = Highlight `nama saya adalah ${nama} umur saya ${umur},${email}` // ini akan di ubah menjadi array dari nilai paramter pertama akan menampung string nya di pisahkand engan expression nya
+
+document.body.innerHTML = str
+console.log(str)
